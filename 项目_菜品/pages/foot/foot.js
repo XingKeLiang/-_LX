819f4fbffa887fc018da3471e23f2f00685901bf
XingKeLiang <372745303@qq.com>
2019-03-27 00:22:06 +0800

    小程序数据

---
+++ 项目_菜品/pages/foot/foot.js
@@ -0,0 +1,50 @@
+// pages/courses/courses.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    preImgSrc: "http://a.itying.com/",
+    list:[]
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.requestFootList()
+  },
+  goFootDetail(e){
+    // console.log(e.target.dataset.fid);
+    var fid = e.target.dataset.fid;
+    wx.navigateTo({
+      url: '../fdetail/fdetail?fid='+fid
+    })
+  },
+  requestFootList(){
+    var that = this;
+    wx.request({
+      url: 'http://a.itying.com/api/productlist',
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success:function(res){
+       var r = res.data.result;
+        console.log(res);
+        for(var i=0;i<r.length;i++){
+          for(var j=0;j<r[i].list.length;j++){
+            var rl = r[i].list;
+            rl[j].img_url = rl[j].img_url.replace(/\\/g,'/');
+          }
+        }
+        that.setData({
+          list:res.data.result
+        });
+        console.log(that.data.list);
+      }
+    })
+  }
+
+})
